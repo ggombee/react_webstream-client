7c120f22bdd0aa47f67454c66de494fd98f1e7cf
ggombee <minniemice98@gmail.com>
2021-08-02 20:19:19 +0900

    chore : edit layout

--- src/features/Main/Cart/index.tsx
+++ src/features/Main/Cart/index.tsx
@@ -1,5 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
 import React from 'react';
+import { MainLayout } from '../../../Layout/main';
 
 export const Cart = () => {
-  return <>Cart</>;
+  return (
+    <MainLayout>
+      <div css={CartWrapper}>비트메이커</div>;
+    </MainLayout>
+  );
 };
+
+const CartWrapper = css`
+  margin-left: 185px;
+
+  height: 100vh;
+  top: 0;
+`;
