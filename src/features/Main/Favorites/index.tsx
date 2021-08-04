199543bf5f40fc3ce4a6d7e2089322e2cb99dfc4
ggombee <minniemice98@gmail.com>
2021-08-04 20:19:19 +0900

    chore : edit layout

--- src/features/Main/Favorites/index.tsx
+++ src/features/Main/Favorites/index.tsx
@@ -1,5 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
 import React from 'react';
+import { MainLayout } from '../../../Layout/main';
 
 export const Favorites = () => {
-  return <>favorites</>;
+  return (
+    <MainLayout>
+      <div css={FavoritesWrapper}>비트메이커</div>;
+    </MainLayout>
+  );
 };
+
+const FavoritesWrapper = css`
+  margin-left: 185px;
+  height: 100vh;
+  top: 0;
+`;
