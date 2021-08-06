a307dabffd4048ba435443a18e9ca0e61e9926c0
ggombee <minniemice98@gmail.com>
2021-08-06 20:19:19 +0900

    chore : edit layout

--- src/features/Main/HotTrack/index.tsx
+++ src/features/Main/HotTrack/index.tsx
@@ -1,5 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
 import React from 'react';
+import { MainLayout } from '../../../Layout/main';
 
 export const HotTrack = () => {
-  return <>핫트랙</>;
+  return (
+    <MainLayout>
+      <div css={HotTrackWrapper}>핫트랙</div>
+    </MainLayout>
+  );
 };
+
+const HotTrackWrapper = css`
+  margin-left: 185px;
+  height: 100vh;
+  top: 0;
+`;
