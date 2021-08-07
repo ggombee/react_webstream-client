5b3ab67abf27b4673f939871b04c5715fa3960e7
ggombee <eunbko@actbase.io>
2021-08-07 20:19:19 +0900

    타입수정

--- src/pages/BeatMaker/index.tsx
+++ src/pages/BeatMaker/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { css } from '@emotion/react'
+import { DUMMY_MAKER_DATA } from 'data/dummy/DUMMY_MAKERS_DATA'
 
 const BeatMaker = () => {
   return (
     <div css={Container}>
-      <div>it's BeatMaker</div>
+      <div css={beatMakerWrapper}>
+        <div css={titleWrapper}>
+          <p css={title}>Beat Maker</p>
+        </div>
+        <div css={content}>
+          
+        </div>
+      </div>
     </div>
   )
 }
@@ -13,6 +21,37 @@ const Container = css`
   display: flex;
   flex: 1;
   overflow: auto;
+`
+
+
+const beatMakerWrapper = css`
+  padding-left: 13px;
+  width: 100%;
+`
+
+const titleWrapper = css`
+  display: flex;
+  align-items: center;
+  button {
+    background-color: transparent;
+    font-size: 15px;
+    line-height: 36px;
+    color: #ffffff;
+    border: none;
+    margin-left: 40px;
+    text-align: center;
+  }
+`
+
+const title = css`
+  font-size: 26px;
+  line-height: 36px;
+  color: #ffffff;
+`
+
+const content  = css`
+  width: 100%; 
+  margin-top: 30px;
 `
 
 export default BeatMaker
