4f5763857771cb09f6280298caf9106c905eed11
ggombee <minniemice98@gmail.com>
2021-08-07 20:19:19 +0900

    chore : edit layout

--- src/features/Main/PlayList/index.tsx
+++ src/features/Main/PlayList/index.tsx
@@ -1,5 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
 import React from 'react';
+import { MainLayout } from '../../../Layout/main';
 
 export const PlayList = () => {
-  return <>playlist</>;
+  return (
+    <MainLayout>
+      <div css={PlayListWrapper}>비트메이커</div>;
+    </MainLayout>
+  );
 };
+
+const PlayListWrapper = css`
+  margin-left: 185px;
+
+  height: 100vh;
+  top: 0;
+`;
