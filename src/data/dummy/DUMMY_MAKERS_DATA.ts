758e052d216aa6ccc185ed0951869d525bc4178a
ggombee <eunbko@actbase.io>
2021-08-07 20:19:19 +0900

    타입수정

---
+++ src/data/dummy/DUMMY_MAKERS_DATA.ts
@@ -0,0 +1,649 @@
+import  { ITrack }  from "./DUMMY_TRACK_DATA"
+import  { Mood }  from "./DUMMY_TRACK_DATA"
+
+
+export interface Maker  {
+  id: number,
+  makerName: string,
+  makerImg?: string,
+  bestTrackInfo?: ITrack[],
+  isFollowed?: boolean,
+  totalBeats?: number,
+  totalFollower?: string,
+  moodInfo?: Mood[]
+}
+
+
+export const DUMMY_MAKER_DATA: Maker[] = [
+  {
+    id: 1,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 2,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: false,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 3,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 4,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 5,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 6,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 7,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 8,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+  {
+    id: 9,
+    makerName: 'Kely Bear',
+    makerImg: '',
+    bestTrackInfo: [
+      { 
+        id: 1, 
+        trackimg: '', 
+        name: 'Stay', 
+        genre: 'POP',
+        makerInfo:{
+          id : 1,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+      { 
+        id: 2, 
+        trackimg: '', 
+        name: 'Levitating', 
+        genre: 'POP',
+        makerInfo:{
+          id : 2,
+          makerName : 'Kely Bear',
+        },
+        mood: [
+          {
+            id : 1,
+            moodname : 'happy'
+          },
+          {
+            id : 2,
+            moodname : 'sexy'
+          }
+        ],
+        isLiked: false,
+        price: 40000,
+        addPlaylist: false,
+      },
+    ],
+    isFollowed: true,
+    totalBeats: 1053,
+    totalFollower: '4.3K',
+    moodInfo: [
+      {
+        id : 1,
+        moodname : 'Happy'
+      },
+      {
+        id : 2,
+        moodname : 'Sexy'
+      },
+      {
+        id : 3,
+        moodname : 'Cozy'
+      }
+    ]
+  },
+]
+  
