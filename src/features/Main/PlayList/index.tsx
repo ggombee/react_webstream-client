011c456205e0940e9f89d744d0f2c95335322601
ggombee <minniemice98@gmail.com>
2021-08-26 20:19:19 +0900

    chore: add coming image

--- src/features/Main/PlayList/index.tsx
+++ src/features/Main/PlayList/index.tsx
@@ -2,18 +2,24 @@
 import { jsx, css } from '@emotion/react';
 import React from 'react';
 import { MainLayout } from '../../../Layout/main';
+import ComingImg from '../../../assets/Main/comingsoon.jpg';
 
 export const PlayList = () => {
   return (
     <MainLayout>
-      <div css={PlayListWrapper}>비트메이커</div>;
+      <div css={PlayListWrapper}>
+        <img src={ComingImg} />
+      </div>
     </MainLayout>
   );
 };
 
 const PlayListWrapper = css`
   margin-left: 185px;
-
   height: 100vh;
   top: 0;
+  img {
+    margin-left: 600px;
+    margin-top: 275px;
+  }
 `;
