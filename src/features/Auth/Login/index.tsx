54f25ce41582c7fd8cd367b125524409bae21597
ggombee <minniemice98@gmail.com>
2021-09-07 20:19:19 +0900

    chore: add image files

--- src/features/Auth/Login/index.tsx
+++ src/features/Auth/Login/index.tsx
@@ -2,9 +2,15 @@
 import { jsx, css } from '@emotion/react';
 import React, { useState } from 'react';
 import LogoImg from '../../../assets/Main/logo.png';
-import ArrowIcon from '../../../assets/Main/arrow_back.png';
+import GoHomeImg from '../../../assets/Main/gohome_rec.png';
 import LoginCheck from '../../../assets/Main/login_check.png';
 import LoginUncheck from '../../../assets/Main/login_uncheck.png';
+import SelectedTrack from '../../../assets/Main/track_selected.png';
+import FacebookImg from '../../../assets/Main/facebook.png';
+import GoogleImg from '../../../assets/Main/google.png';
+import NaverImg from '../../../assets/Main/naver.png';
+import KakaoImg from '../../../assets/Main/kakao.png';
+import LoginImg from '../../../assets/Main/login_hover.png';
 
 import { AuthLayout } from '../../../Layout/auth';
 import { useHistory } from 'react-router';
@@ -24,8 +30,11 @@ import { number } from 'prop-types';
 export const Login = () => {
   const history = useHistory();
   const [isActive, setIsActive] = useState(false);
+  const [isCategory, setIsCategory] = useState(1);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleClickMove = (no: number) => {
+  const handleClickButton = (no: number) => {
     if (no === 0) {
       history.goBack();
     } else {
@@ -40,7 +49,7 @@ export const Login = () => {
   return (
     <AuthLayout>
       <div css={LoginWrapper}>
-        <div css={BackButton} onClick={() => handleClickMove(0)}>
+        <div css={BackButton} onClick={() => handleClickButton(0)}>
           <button
             css={css`
               border: none;
@@ -50,67 +59,190 @@ export const Login = () => {
               margin-left: 10px;
             `}
           >
-            <img src={ArrowIcon} />
-            <a>메인화면으로 돌아가기</a>
+            <img />
+            <a>Go Home</a>
           </button>
         </div>
-        <div css={LoginForm}>
+        <div>
           <img
             src={LogoImg}
             css={css`
-              margin-left: 50px;
-              margin-right: 50px;
-              width: 200px;
+              position: absolute;
+              width: 175.3px;
+              height: 40.08px;
+              left: 872px;
+              top: 174px;
             `}
           />
           <div
+            onClick={() => handleClickButton(1)}
             css={css`
-              justify-content: space-between;
+              display: block;
+              position: absolute;
+              width: 51px;
+              height: 21px;
+              left: 770px;
+              top: 309px;
+              font-family: Poppins;
+              font-style: normal;
+              font-weight: 600;
+              font-size: 14px;
+              line-height: 21px;
+              color: #ffffff;
             `}
           >
-            <input
-              placeholder="아이디"
+            <span
               css={css`
-                width: 140px;
+                margin-right: 70px;
+                width: 51px;
+                display: block;
+                cursor: pointer;
               `}
-            />
-            <a>@</a>
-            <select>
-              <option value="" disabled selected hidden>
-                도메인 선택
-              </option>
-              <option value="0">naver.com</option>
-              <option value="1">nate.com</option>
-              <option value="2">daum.com</option>
-            </select>
-          </div>
-          <input
-            placeholder="비밀번호"
-            css={css`
-              width: 310px;
-            `}
-          />
-          <div>
-            <button
-              css={css`
-                background: #9354ed;
-                border: none;
-                border-radius: 80px;
-                margin-top: 10px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #ffffff;
-              `}
-              onClick={() => handleClickMove(1)}
+              style={isCategory === 1 ? { color: '#ffffff' } : { color: '#5e6266' }}
             >
-              로그인
-            </button>
+              Sign in
+            </span>
+            {isCategory === 1 && (
+              <img
+                src={SelectedTrack}
+                css={css`
+                  display: block;
+                  width: 48px;
+                  height: 2px;
+                  background: #f6112d;
+                  margin-top: 5px;
+                  cursor: pointer;
+                `}
+              />
+            )}
+          </div>
+          <div
+            onClick={() => handleClickButton(2)}
+            css={css`
+              display: block;
+              position: absolute;
+              width: 52px;
+              height: 21px;
+              left: 861px;
+              top: 309px;
+              font-family: Poppins;
+              font-style: normal;
+              font-weight: 600;
+              font-size: 14px;
+              line-height: 21px;
+
+              color: #ffffff;
+            `}
+          >
+            <span
+              css={css`
+                margin-right: 70px;
+                width: 52px;
+                display: block;
+                cursor: pointer;
+              `}
+              style={isCategory === 2 ? { color: '#ffffff' } : { color: '#5e6266' }}
+            >
+              Sign up
+            </span>
+            {isCategory === 2 && (
+              <img
+                src={SelectedTrack}
+                css={css`
+                  display: block;
+                  width: 52px;
+                  height: 2px;
+                  background: #f6112d;
+                  margin-top: 5px;
+                  cursor: pointer;
+                `}
+              />
+            )}
           </div>
           <div
             css={css`
-              margin-top: 15px;
-              text-align: center;
+              position: absolute;
+              width: 380px;
+              height: 58px;
+              left: 770px;
+              top: 361px;
+              background: #000000;
+              box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+              border-radius: 5px;
+            `}
+            style={email.length > 0 ? { border: '1px solid #ffffff' } : { color: '#5e6266' }}
+          >
+            <input
+              placeholder="Email"
+              css={css`
+                :focus {
+                  outline: none;
+                }
+                position: absolute;
+                margin-left: 25px;
+                margin-top: 12px;
+                width: 335px;
+                height: 30px;
+                font-family: Poppins;
+                font-style: normal;
+                font-weight: 600;
+                font-size: 16px;
+                line-height: 21px;
+                color: #484848;
+                background: transparent;
+                border: none;
+              `}
+              value={email}
+              style={email.length > 0 ? { color: '#ffffff' } : { color: '#5e6266' }}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div
+            css={css`
+              position: absolute;
+              width: 380px;
+              height: 58px;
+              left: 770px;
+              top: 427px;
+              background: #000000;
+              box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+              border-radius: 5px;
+            `}
+            style={password.length > 0 ? { border: '1px solid #ffffff' } : { color: '#5e6266' }}
+          >
+            <input
+              placeholder="Password"
+              css={css`
+                :focus {
+                  outline: none;
+                }
+
+                position: absolute;
+                margin-left: 25px;
+                margin-top: 12px;
+                width: 335px;
+                height: 30px;
+                font-family: Poppins;
+                font-style: normal;
+                font-weight: 600;
+                font-size: 16px;
+                line-height: 21px;
+                color: #484848;
+                background: transparent;
+                border: none;
+              `}
+              value={password}
+              style={password.length > 0 ? { color: '#ffffff' } : { color: '#5e6266' }}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div
+            css={css`
+              position: absolute;
+              width: 150px;
+              height: 22px;
+              left: 770px;
+              top: 501px;
             `}
             onClick={() => handleCLickKeepLogin(isActive)}
           >
@@ -135,95 +267,80 @@ export const Login = () => {
             <a
               css={css`
                 font-size: 13px;
+                color: #ffffff;
+                width: 90px;
               `}
             >
-              로그인 유지하기
+              Keep me logged in
             </a>
           </div>
-          <div>
-            <button
-              css={css`
-                background: #2478ff;
-                border: none;
-                border-radius: 80px;
-                margin-top: 20px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #ffffff;
-              `}
-              onClick={() => handleClickMove(2)}
-            >
-              Facebook 로그인
-            </button>
+
+          <div css={LoginButton} onClick={() => handleClickButton(3)}>
+            <img />
+            <a>LOGIN NOW</a>
+          </div>
+          <div
+            css={css`
+              position: absolute;
+              width: 18px;
+              height: 23px;
+              left: 951px;
+              top: 613px;
+              font-family: Poppins;
+              font-style: normal;
+              font-weight: 600;
+              font-size: 15px;
+              line-height: 22px;
+              color: #ffffff;
+            `}
+          >
+            or
           </div>
           <div>
-            <button
+            <img
+              src={FacebookImg}
               css={css`
-                background: #ededed;
-                border: none;
-                border-radius: 80px;
-                margin-top: 10px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #000000;
+                position: absolute;
+                width: 390px;
+                height: 70px;
+                left: 770px;
+                top: 652px;
               `}
-              onClick={() => handleClickMove(3)}
-            >
-              Google 로그인
-            </button>
-          </div>
-          <div>
-            <button
+              onClick={() => handleClickButton(4)}
+            />
+            <img
+              src={GoogleImg}
               css={css`
-                background: #ffffff;
-                border: none;
-                border-radius: 80px;
-                margin-top: 10px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #000000;
+                position: absolute;
+                width: 390px;
+                height: 70px;
+                left: 770px;
+                top: 718px;
               `}
-              onClick={() => handleClickMove(4)}
-            >
-              Apple 로그인
-            </button>
-          </div>
-          <div>
-            <button
+              onClick={() => handleClickButton(5)}
+            />
+            <img
+              src={NaverImg}
               css={css`
-                background: #0bc904;
-                border: none;
-                border-radius: 80px;
-                margin-top: 10px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #ffffff;
+                position: absolute;
+                width: 390px;
+                height: 70px;
+                left: 770px;
+                top: 784px;
               `}
-              onClick={() => handleClickMove(5)}
-            >
-              Naver 로그인
-            </button>
-          </div>
-          <div>
-            <button
+              onClick={() => handleClickButton(6)}
+            />
+            <img
+              src={KakaoImg}
               css={css`
-                background: #fff612;
-                border: none;
-                border-radius: 80px;
-                margin-top: 10px;
-                width: 310px;
-                font-size: 13px;
-                height: 29px;
-                color: #000000;
+                position: absolute;
+                width: 390px;
+                height: 70px;
+                left: 770px;
+                top: 850px;
               `}
-              onClick={() => handleClickMove(6)}
-            >
-              Kakao 로그인
-            </button>
+              onClick={() => handleClickButton(7)}
+            />
           </div>
         </div>
       </div>
@@ -241,42 +358,75 @@ const BackButton = css`
   margin: 0;
   border: none;
   color: #ffffff;
+  :hover {
+    img {
+      background: url(${SelectedTrack});
+    }
+  }
   img {
-    vertical-align: middle;
+    position: absolute;
+    width: 136px;
+    height: 2px;
+    left: 0px;
+    top: 84px;
+    background: url(${GoHomeImg});
   }
   a {
-    margin-left: 5px;
+    cursor: pointer;
+    position: absolute;
+    width: 66px;
+    height: 21px;
+    left: 70px;
+    top: 58px;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 21px;
+    color: #ffffff;
   }
 `;
 
-const LoginForm = css`
-  color: #5e6266;
-  display: block;
-  margin-top: 150px;
-  margin-left: 750px;
-  input {
-    margin-top: 10px;
-    border: none;
-    background: #151515;
-    height: 26px;
-    border-radius: 80px;
-    color: #5e6266;
-    font-size: 13px;
+const LoginButton = css`
+  position: absolute;
+  width: 380px;
+  height: 58px;
+  left: 770px;
+  top: 539px;
+  background: #000000;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  display: flex;
+  cursor: pointer;
+  :hover {
+    background: #141515;
+    color: #ffffff;
+    border: 1px solid #f6112d;
+    a {
+      color: #ffffff;
+    }
+    img {
+      background: url(${LoginImg});
+    }
+  }
+
+  img {
+    margin: 29px 0px 0px 40px;
+    width: 160px;
+    height: 2px;
+    background: url(${GoHomeImg});
   }
 
   a {
-    margin-top: 100px;
-    margin-left: 3px;
-  }
-  select {
-    margin-left: 5px;
-    width: 145px;
-    border: none;
-    font-size: 13px;
-    margin-top: 100px;
-    background: #151515;
-    height: 26px;
-    border-radius: 80px;
-    color: #5e6266;
+    margin: 17px 0px 0px 40px;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 0.05em;
+    color: #484848;
   }
 `;
