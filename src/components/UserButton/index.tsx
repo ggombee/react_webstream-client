df1d5a4c8694e544939bcc80ff5be9d5a209db86
ggombee <eunbko@actbase.io>
2021-10-02 20:19:19 +0900

    페이지 제작중

---
+++ src/components/UserButton/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { css } from '@emotion/react'
+import StarIcon from '~/assets/icons/stars.png'
+import DefaultProfile from '~/assets/sample/default_user.png'
+
+const UserButton = () => {
+  return (
+    <div css={container}>
+      <div css={userBadge}>
+        <img src={StarIcon} alt={'badge'} />
+      </div>
+      <div css={userName}>
+        <span>Bob</span>
+      </div>
+      <div css={userPicture}>
+        <img src={DefaultProfile} alt={'user'} />
+      </div>
+    </div>
+  )
+}
+
+export default UserButton
+
+const container = css`
+  float: right;
+  margin-right: 60px;
+  display: inline-flex;
+  align-items: center;
+  color: #fff;
+`
+const userBadge = css``
+
+const userName = css`
+  margin-left: 6px;
+  font-family: AppleSDGothicNeoB00;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 17px;
+`
+
+const userPicture = css`
+  margin-left: 22px;
+`
