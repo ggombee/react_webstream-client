f7d59d1058a96752fcfe4cfa49c35ef04325b4cf
ggombee <eunbko@actbase.io>
2021-11-09 18:53:06 +0900

    로그인 페이지 작업중

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,56 @@
 import React from 'react'
+import { css } from '@emotion/react'
+
 import styled from '@emotion/styled'
 import Background from '~/assets/background/background.png'
 
 const Login = () => {
-  return <Container>it's Login Page</Container>
+  return (
+    <div css={container}>
+      <button css={backWrapper}>
+        <div css={word}>GO HOME</div>
+      </button>
+      <div css={loginWrapper}>
+        <div css={logoWrapper}>dd</div>
+      </div>
+    </div>
+  )
 }
 
-const Container = styled.div`
+const container = css`
   display: flex;
   flex: 1;
   overflow: auto;
   background-image: url(${Background});
+`
+
+const backWrapper = css`
+  margin-top: 58px;
+  width: 140px;
+  height: 21px;
+  border-bottom: solid 2px #474747;
+  :hover {
+    border-bottom: solid 2px red;
+  }
+`
+const word = css`
+  color: white;
+  margin-left: 70px;
+  width: 66px;
+`
+
+const loginWrapper = css`
+  display: flex;
+  justify-content: center;
+  direction: vertical;
+  width: 150px;
+  height: 800px;
+  background-color: white;
+  margin-top: 175px;
+`
+
+const logoWrapper = css`
+  color: white;
 `
 
 export default Login
