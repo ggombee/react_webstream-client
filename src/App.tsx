e3ea7173383bc2c2667e1f88ab967a47d22890ca
ggombee <eunbko@actbase.io>
2021-11-22 19:00:31 +0900

    회원가입 약관/가입 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import Home from '~/pages/Home'
-import Login from '~/pages/Login'
+import Auth from '~/pages/Auth'
 import HotTrack from '~/pages/HotTrack'
 import BeatMaker from './pages/BeatMaker'
 import Favorites from './pages/Favorites'
@@ -9,13 +9,17 @@ import Playlist from './pages/Playlist'
 import LayoutRoute from './components/LayoutRoutes'
 import Default from '~/layouts/Default'
 import Blank from './layouts/Blank'
+import Register from './pages/Register'
 
 function App() {
   return (
     <Router>
       <Switch>
         <LayoutRoute exact path="/" layout={Default} component={Home} />
-        <LayoutRoute exact path="/login" layout={Blank} component={Login} />
+        <LayoutRoute exact path="/auth/login" layout={Blank} component={Auth} />
+        <LayoutRoute path="/auth/terms" layout={Blank} component={Auth} />
+        <LayoutRoute path="/auth/register" layout={Blank} component={Auth} />
+
         <LayoutRoute
           exact
           path="/hottrack"
